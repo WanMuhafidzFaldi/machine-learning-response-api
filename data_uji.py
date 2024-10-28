20debe5d941da953ecd457a0194c971fa8347bb3
Wan Muhafidz Faldi <wanmuhafidzfaldi@gmail.com>
2024-10-28 09:44:19 +0700

    Update data uji and data make to a different file

---
+++ data_uji.py
@@ -0,0 +1,27 @@
+# 1. Contoh DataUji  
+dataUji = [
+    {"responseData": '{"email": "user@example.com"}', "scoringData": 2},  
+    {"responseData": '{"pic": "john@company.com"}', "scoringData": 2},  
+    {"responseData": '{"alamat": "Jakarta Selatan"}', "scoringData": 2},  
+    {"responseData": '{"fullName": "Ahmad"}', "scoringData": 2},  
+    {"responseData": '{"phoneNumber": "08512312312"}', "scoringData": 2},
+    {"responseData": '{"email": "00542025-754c-4446-9030-22758938cc3b"}', "scoringData": 1},
+    {"responseData": '{"productName": "Mangga Goreng"}', "scoringData": 0},
+    {"responseData": '{"productName": "Mangga Goreng"}', "scoringData": 0},
+    {"responseData": '{"email": "00542025-754c-4446-9030-22758938cc3b"}', "scoringData": 1},
+    {"responseData": '{"fullName": "Ahmad"}', "scoringData": 2},
+    {"responseData": '{"productName": "Apel Mentega"}', "scoringData": 0},
+    {"responseData": '{"email": "44423456-7890-4567-8901-345678901234"}', "scoringData": 1},
+    {"responseData": '{"fullName": "Budi"}', "scoringData": 2},
+    {"responseData": '{"productName": "Pisang Keju"}', "scoringData": 0},
+    {"responseData": '{"email": "55534567-8901-4567-8901-456789012345"}', "scoringData": 1},
+    {"responseData": '{"fullName": "Citra"}', "scoringData": 2},
+    {"responseData": '{"productName": "Nasi Goreng"}', "scoringData": 0},
+    {"responseData": '{"email": "66645678-9012-4567-8901-567890123456"}', "scoringData": 1},
+    {"responseData": '{"fullName": "Denny"}', "scoringData": 2},
+    {"responseData": '{"scene":-699625218,"along":1278992754,"start":true,"mind":"mark","perhaps":{"knife":1854340532,"stove":{"advice":true,"him":"exciting","selection":"roll","seeing":true,"slipped":-1818490715.8282523,"brave":false},"occasionally":"supper","single":"pan","threw":true,"volume":"kill"},"pressure":"apart"}', "scoringData": 0},
+    {"responseData": '{"known":1435401701.0544872,"most":{"neck":-1081584193.9818058,"knowledge":"negative","art":{"construction":-1029749549,"bound":true,"field":"flame","no":true,"win":260693246,"very":"youth"},"younger":"early","mean":-2048932268,"below":false},"jack":"replace","wing":"outer","outline":"excellent","nearby":true}', "scoringData": 0},
+    {"responseData": '{"damage":"be","birth":"mirror","speech":true,"grandfather":true,"forgotten":"individual","guess":"company"}', "scoringData": 0},
+    {"responseData": '{"steel":"knife","anybody":"environment","dirt":"afternoon","twenty":true,"lady":false,"struck":"compound"}', "scoringData": 0},
+    # Tambahkan lebih banyak data sesuai kebutuhan  
+]  
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json 
-from sklearn.svm import LinearSVC  
-from sklearn.metrics import classification_report  
 from data_processing import dataProcessing
 from data_set import dataSet
+from data_uji import dataUji
 from machine_learning import machineLearning
 from machine_learning import predictPii
 
@@ -10,41 +8,5 @@ xTrain, xTest, yTrain, yTest, vectorizer = dataProcessing(dataSet)
 
 model = machineLearning(xTrain, xTest, yTrain, yTest)
 
-
-
-# # Contoh Penggunaan  
-new_response_1 = '''  
-{  
-  "products": [  
-    {  
-      "id": 101,  
-      "title": "Apple AirPods Max Silver",  
-      "category": "mobile-accessories"  
-    }  
-  ],  
-  "total": 23,  
-  "skip": 0,  
-  "limit": 23  
-}  
-'''  
-
-new_response_2 = '''  
-{  
-  "email": "asdfasdfdsfds",  
-  "fullName": "asdfasdf",  
-  "phoneNumber": "asdfasfdsfs",  
-  "products": [  
-    {  
-      "id": 101,  
-      "title": "Apple AirPods Max Silver",  
-      "category": "mobile-accessories"  
-    }  
-  ],  
-  "total": 23,  
-  "skip": 0,  
-  "limit": 23  
-}  
-''' 
-
-print(predictPii(model, vectorizer, new_response_1))  # Output: Does not contain PII  
-print(predictPii(model, vectorizer, new_response_2))  # Output: Contains PII  
+for data in dataUji:
+    print(predictPii(model, vectorizer, data['responseData']))  # Output: Does not contain PII
