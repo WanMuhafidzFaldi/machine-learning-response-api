92ace7336c9eb6103f678c0238ace431d659c883
Wan Muhafidz Faldi <wanmuhafidzfaldi@gmail.com>
2024-11-21 19:31:05 +0700

    change algorithm from not flatten to flatten data

--- project/utils/data_uji.py
+++ project/utils/data_uji.py
@@ -1,5 +1,17 @@
 import json
 
+
+def flattenDict(d, parent_key='', sep='.'):
+    items = []
+    for k, v in d.items():
+        new_key = f"{parent_key}{sep}{k}" if parent_key else k
+        if isinstance(v, dict):
+            items.extend(flattenDict(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
+
 def loadDataTest(filePath):
     with open(filePath, 'r') as file:
         data = json.load(file)
@@ -15,10 +27,16 @@ def loadDataTest(filePath):
         # Ubah responseData menjadi string JSON
         if isinstance(responseData, (list, dict)):
           responseData = json.dumps(responseData)
-  
-        dataTest.append({
-          'responseData': responseData,
-          'scoringData': maxScoringData
-        })
+
+        # Parse responseData back to a dictionary if it's a JSON string
+        responseData = json.loads(responseData)
+        
+        # Flatten the responseData
+        flattenedResponseData = flattenDict(responseData)
+        for key, value in flattenedResponseData.items():
+          dataTest.append({
+              'responseData': json.dumps({key: value}),
+              'scoringData': maxScoringData
+          })
 
     return dataTest
